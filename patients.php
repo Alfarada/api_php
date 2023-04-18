2599667471385d3bdc804562f1bd065c1697257e
Alfredo Yepez <alfaradayepez@gmail.com>
2023-04-18 09:31:32 -0500

    no longer nedded

--- patients.php
+++ patients.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
@@ -15,15 +15,37 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if (isset($_GET['page'])) {
         $page = (int) $_GET['page'];
         $list = $_patients->patientsList($page);
+        header('Content-type: application/json');
         echo json_encode($list);
-    } else if (isset($_GET['id'])){
+        http_response_code(200);
+    } else if (isset($_GET['id'])) {
         $patient_id = $_GET['id'];
         $patient_data = $_patients->getPatient($patient_id);
-        echo "<pre>" . json_encode($patient_data) . "</pre>";
+        header('Content-type: application/json');
+        echo json_encode($patient_data);
+        http_response_code(200);
+    }
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // recibimos los datos 
+    $post_body = file_get_contents("php://input");
+    // manejador
+    $array_data = $_patients->post($post_body);
+
+    // print_r($response);
+
+    //devolvemos una respuesta 
+    header('Content-type: application/json');
+
+    if (isset($array_data['result']['error_id'])) {
+        $response_code = $array_data['result']['error_id'];
+        http_response_code($response_code);
+    } else {
+        http_response_code(200);
     }
 
-} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo 'hola post';
+    echo json_encode($array_data);
+
 } else if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
     echo 'hola put';
 } else if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
