e0d00e02bd1486eb054a93c41f913eb47fe66b30
Alfredo Yepez <alfaradayepez@gmail.com>
2023-04-18 09:28:16 -0500

    add post method

--- class/patients.php
+++ class/patients.php
@@ -9,6 +9,16 @@ class patients extends conection
 {
 
     private string $table = 'pacientes';
+    private $patientsId = "";
+    private $dni = "";
+    private $nombre = "";
+    private $direccion = "";
+    private $codigoPostal = "";
+    private $genero = "";
+    private $telefono = "";
+    private $fechaNacimiento = "0000-00-00";
+    private $correo = "";
+
 
     public function patientsList(int $page = 0): array
     {
@@ -29,5 +39,75 @@ class patients extends conection
     {
         $sql = "SELECT * FROM $this->table WHERE PacienteId = '$id'";
         return $this->getData($sql);
+    }
+
+    /**
+     *  metodo post
+     * 
+     * Este manejador valida los datos requeridos
+     * y luego se hace un insert a la base de datos
+     */
+    public function post($json)
+    {
+        $_response = new Response();
+        // convertimos a matriz asociativa
+        $data = json_decode($json, true);
+
+        // campos requridos
+        if (!isset($data['nombre']) || !isset($data['dni']) || !isset($data['correo'])) {
+            return $_response->http_status_400();
+        } else {
+
+            // no es necesario validar
+            $this->nombre = $data['nombre'];
+            $this->dni = $data['dni'];
+            $this->correo = $data['correo'];
+
+            // validando campos 
+            if (isset($data['telefono'])) {
+                $this->telefono = $data['telefono'];
+            }
+            if (isset($data['direccion'])) {
+                $this->direccion = $data['direccion'];
+            }
+            if (isset($data['codigoPostal'])) {
+                $this->codigoPostal = $data['codigoPostal'];
+            }
+            if (isset($data['genero'])) {
+                $this->genero = $data['genero'];
+            }
+            if (isset($data['fechaNacimiento'])) {
+                $this->fechaNacimiento = $data['fechaNacimiento'];
+            }
+
+            $insertId = $this->insertPatients();
+            // var_dump($insertId);
+
+            if ($insertId) {
+                $response = $_response->response;
+                $response['result'] = [
+                    "pacienteId" => $insertId,
+                ];
+
+                return $response;
+            } else {
+
+                return $_response->http_status_500();
+            }
+        }
+    }
+
+    public function insertPatients() :int {
+        $query = "INSERT INTO {$this->table} (DNI, Nombre, Direccion, CodigoPostal, Telefono, Genero, FechaNacimiento, Correo)
+        VALUES
+        ('$this->dni', '$this->nombre', '$this->direccion', '$this->codigoPostal', '$this->telefono', '$this->genero', '$this->fechaNacimiento', '$this->correo')";
+
+        $response = $this->nonQuery($query);
+
+        if ($response) {
+            return $response;
+        } else {
+            return 0;
+        }
     }
 }
